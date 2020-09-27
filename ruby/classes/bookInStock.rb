e616459947eaadf55d96ced2515e31e9d5bf7215
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2020-09-27 09:27:42 +0400

    create class and override to_s method

---
+++ ruby/classes/bookInStock.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+class BookInStock
+  def initialize(isbn, price)
+    @isbn = isbn
+    @price = Float(price)
+  end
+
+  def to_s
+    "ISBN: #{@isbn} , price #{@price}"
+  end
+end
+
+b1 = BookInStock.new('isbn1', 3)
+p b1
+puts b1
+b2 = BookInStock.new('isbn2', 3.14)
+p b2
+puts b2
+b3 = BookInStock.new('isbn3', '5.67')
+p b3
+puts b3
