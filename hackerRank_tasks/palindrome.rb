35f42013abd3f8e71caf97a924d7241c57c2e047
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2020-10-06 19:11:16 +0400

    modify palindrome task

--- hackerRank_tasks/palindrome.rb
+++ hackerRank_tasks/palindrome.rb
@@ -1,15 +1,6 @@
 def palindromeIndex(s)
   return -1 if s == s.reverse
 
-  count = 0
-
-  s.size.times do |i|
-    count += 1 if s[0..i].chars.none? { |l| l == s[i] }
-    p 'dfdfd'
-  end
-
-  return - 1 if count > 4 and s.length == count+1
-
   s.size.times do |i|
     temp = s[0...i] << s[i + 1..-1]
     return i if temp == temp.reverse
@@ -18,4 +9,4 @@ def palindromeIndex(s)
   -1
 end
 
-p palindromeIndex('allah')
+p palindromeIndex('silent')
