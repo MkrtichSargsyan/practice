5ad6abec2dfc01e23c4804b9f6a02644f356f24f
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2020-10-12 01:34:41 +0400

    edited show and create actions and templates to display the blog input.

--- rails/Blog/app/controllers/articles_controller.rb
+++ rails/Blog/app/controllers/articles_controller.rb
@@ -1,9 +1,20 @@
 class ArticlesController < ApplicationController
-  def new
-    
+  def new; end
+
+  def show
+    @article = Article.find(params[:id])
   end
 
   def create
-    render plain: params[:article].inspect
+    # render json: params[:article]
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to @article
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :text)
   end
 end
