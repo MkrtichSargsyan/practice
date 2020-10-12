df1894c4a51ec422ebeb4d38ee1ccaed78027aef
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2020-10-12 18:42:46 +0400

    Added being able to  edit and delete article

--- rails/Blog/app/controllers/articles_controller.rb
+++ rails/Blog/app/controllers/articles_controller.rb
@@ -10,11 +10,31 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
   def create
     # render json: params[:article]
     @article = Article.new(article_params)
     @article.save
     redirect_to @article
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
   end
 
   private
