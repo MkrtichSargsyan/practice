b04c112dacf388a34067a0b2a9e5b208f5554e52
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2020-11-06 23:17:27 +0400

    start chocolatefeast task

---
+++ hackerRank_tasks/chocolate_feast.rb
@@ -0,0 +1,4 @@
+def chocolateFeast(n, c, m)
+
+  Complete the chocolateFeast function in the editor below. It must return the number of chocolates Bobby can eat after taking full advantage of the promotion.
+end
