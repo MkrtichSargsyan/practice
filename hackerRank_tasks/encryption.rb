12814cd95d47bb647dca03070790e60b6749ebc4
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2020-11-06 22:15:24 +0400

    solve encryption task

---
+++ hackerRank_tasks/encryption.rb
@@ -0,0 +1,27 @@
+def encryption(s)
+  word = s.split(' ').join
+  count = word.chars.count
+  cols = Math.sqrt(count).ceil
+
+  str = ''
+  word.chars.each_with_index do |el, ind|
+    str += ' ' if (ind % cols).zero? && (ind != 0)
+    str += el
+  end
+
+  p str
+
+  arr = str.split(' ')
+
+  temp =''
+
+  cols.times do |t|
+    arr.each do |item|
+      temp += item[t] if !item[t].nil?
+    end
+    temp += ' '
+  end
+  temp
+end
+
+p encryption('have a nice day')
