8defcc6a7d8b35d204502debc673b43e615f7f8d
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2020-11-08 20:49:31 +0400

    start manasa and stones

---
+++ hackerRank_tasks/manasa_and_stones.rb
@@ -0,0 +1,4 @@
+def stones(n, a, b)
+
+  Complete the stones function in the editor below. It should return an array of integers representing all possible values of the last stone, sorted ascending.
+end
