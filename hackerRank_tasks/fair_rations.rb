d2ce885e07140167f994a3f61a487fbbd905a42b
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2020-11-08 20:46:54 +0400

    start fair rations

---
+++ hackerRank_tasks/fair_rations.rb
@@ -0,0 +1,4 @@
+def fairRations(B)
+
+  You are the benevolent ruler of Rankhacker Castle, and today you're distributing bread. Your subjects are in a line, and some of them already have some loaves. Times are hard and your castle's food stocks are dwindling, so you must distribute as few loaves as possible according to the following rules:
+end
