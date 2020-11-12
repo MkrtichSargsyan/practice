2b7ec0881d91ac803b0cc41865f1e387dc75f270
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2020-11-12 22:54:43 +0400

    start insertion sort2

---
+++ hackerRank_tasks/insertion_sort2.rb
@@ -0,0 +1,4 @@
+def insertionSort2(n, arr)
+
+  Complete the insertionSort2 function in the editor below. At each iteration, it should print the array as space-separated integers on a separate line.
+end
