824c8e32600ce1f4eedfb80f8ed63134e2ab8e88
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2020-11-12 22:56:48 +0400

    start bill division task

---
+++ hackerRank_tasks/bill_division.rb
@@ -0,0 +1,4 @@
+def bonAppetit(bill, k, b)
+
+  Complete the bonAppetit function in the editor below. It should print Bon Appetit if the bill is fairly split. Otherwise, it should print the integer amount of money that Brian owes Anna.
+end
