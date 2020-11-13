c4e04167ebc5a9862e703b25e02ec5a563105053
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2020-11-13 22:48:57 +0400

    change happyLady bbugs method

--- hackerRank_tasks/cavity_map.rb
+++ hackerRank_tasks/cavity_map.rb
@@ -1,4 +1,3 @@
 def cavityMap(grid)
 
-  Find all the cavities on the map and replace their depths with the uppercase character X.
 end
--- hackerRank_tasks/happy_ladybugs.rb
+++ hackerRank_tasks/happy_ladybugs.rb
@@ -1,4 +1,15 @@
 def happyLadybugs(b)
-Complete the happyLadybugs function in the editor below. It should return an array of strings, either 'YES' or 'NO', one for each test string.
 
+
+  sorted_b = b.split('').sort
+
+  return 'NO' if sorted_b[0] != sorted_b[1]
+
+  mid_b = sorted_b[1...-1]
+  mid_b.each_with_index do |el, ind|
+    
+    return 'NO' if el != mid_b[ind - 1] || el != [ind + 1]
+  end
 end
+
+p happyLadybugs('RBY_YBR')
