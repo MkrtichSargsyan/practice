16f8c195b8bf06d6374355b75be1f4c31f590213
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2020-12-29 09:12:24 +0400

    solve happy ladybugs

--- hackerRank_tasks/happy_ladybugs.rb
+++ hackerRank_tasks/happy_ladybugs.rb
@@ -1,15 +1,21 @@
 def happyLadybugs(b)
-
-
   sorted_b = b.split('').sort
+  output = 'NO'
+  p sorted_b
 
-  return 'NO' if sorted_b[0] != sorted_b[1]
+  if sorted_b[-1] == '_'
+    output = 'YES'
+    return output if sorted_b[0] == '_'
 
-  mid_b = sorted_b[1...-1]
-  mid_b.each_with_index do |el, ind|
-    
-    return 'NO' if el != mid_b[ind - 1] || el != [ind + 1]
+    sorted_b.each do |el|
+      return 'NO' if sorted_b.select { |letter| el == letter && el != '_' }.count == 1
+    end
+
+  else
+    return 'YES' if sorted_b == b
   end
+
+  output
 end
 
-p happyLadybugs('RBY_YBR')
+p happyLadybugs('_')
