bf41cde1b93ce23b8ff121992eab3c3aa030a904
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2021-02-25 02:30:31 +0400

    solve divisible sum pairs

--- hackerRank_tasks/divisible_sum_pairs.rb
+++ hackerRank_tasks/divisible_sum_pairs.rb
@@ -1,4 +1,13 @@
-def divisibleSumPairs(n, k, ar)
-  Complete the divisibleSumPairs function in the editor below. It should return the integer count of pairs meeting the criteria.
+# frozen_string_literal: true
 
+def divisibleSumPairs(_n, k, ar)
+  count = 0
+  ar.each_with_index do |el1, i|
+    ar.each_with_index do |el2, j|
+      count += 1 if ((el1 + el2) % k == 0) && (j > i)
+    end
+  end
+  count
 end
+
+p divisibleSumPairs(6, 3, [1, 3, 2, 6, 1, 2])
