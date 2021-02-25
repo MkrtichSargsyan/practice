37596204bb9643fa8eb09c27592203519140a124
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2021-02-26 02:03:57 +0400

    solve bill division

--- hackerRank_tasks/bill_division.rb
+++ hackerRank_tasks/bill_division.rb
@@ -1,4 +1,10 @@
-def bonAppetit(bill, k, b)
+# frozen_string_literal: true
 
-  Complete the bonAppetit function in the editor below. It should print Bon Appetit if the bill is fairly split. Otherwise, it should print the integer amount of money that Brian owes Anna.
+def bonAppetit(bill, k, b)
+  bill.delete_at(k)
+  actual_bill = bill.inject(0) { |result, cur| result + cur } / 2
+
+  print actual_bill == b ? "Bon Appetit" : b - actual_bill
 end
+
+bonAppetit([3, 10, 2, 9], 1, 7)
