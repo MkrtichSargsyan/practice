84dafe87dba0425c99e5ffbfe63b99e55ce66215
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2021-02-25 20:36:01 +0400

    solve insertion sort 1

--- hackerRank_tasks/insertion_sort.rb
+++ hackerRank_tasks/insertion_sort.rb
@@ -1,4 +1,27 @@
-def insertionSort1(n, arr)
+# frozen_string_literal: true
 
-  Print the array as a row of space-separated integers each time there is a shift or insertion.
+def insertionSort1(n, arr)
+  i = n - 1
+  target = arr[n - 1]
+  stop = false
+  while !stop && (i > 0)
+
+    if arr[i - 1] > target
+      arr[i] = arr[i - 1]
+      i -= 1
+      arr.length.times do |el|
+        print "#{arr[el]} "
+      end
+      puts 
+    else
+      stop = true
+    end
+  end
+  arr[i] = target
+  arr.length.times do |el|
+    print "#{arr[el]} "
+  end
+  puts
 end
+
+insertionSort1(5, [2, 4, 6, 8, 3])
